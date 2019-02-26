40120861ca91156b27f10de1a5931c7ab646b3f0
Thomas Bille <thomas.bille@canonical.com>
2019-02-26 14:31:47 +0000

    Convert app to flask

--- script.py
+++ script.py
@@ -1,22 +1,19 @@
-from __future__ import print_function
-
 import os
-from wsgiref.simple_server import make_server
+
+import flask
 
 from influxdb import InfluxDBClient
-from pyramid.config import Configurator
-from pyramid.response import Response
-from pyramid.view import view_config, view_defaults
 
-ENDPOINT = "webhook"
+ENDPOINT = "/webhook"
 INFLUXDB_CONFIG = {
-    "HOST": os.getenv("INFLUXDB_HOST", "localhost"),
+    "HOST": os.getenv("INFLUXDB_HOST", "influx-db"),
     "PORT": os.getenv("INFLUXDB_PORT", "8086"),
     "USERNAME": os.getenv("INFLUXDB_USERNAME", ""),
-    "PASSWORD": os.getenv("INFLUXDB_PASSWORD", ""),
+    "PASSWORD": os.getenv(
+        "INFLUXDB_PASSWORD", ""
+    ),
     "DATABASE": os.getenv("INFLUXDB_DATABASE", "githubdb"),
 }
-
 
 client = InfluxDBClient(
     INFLUXDB_CONFIG["HOST"],
@@ -27,12 +24,44 @@ client = InfluxDBClient(
 )
 
 client.create_database("githubdb")
+app = flask.Flask(__name__)
+
+
+@app.route(ENDPOINT, methods=["POST"])
+def webhook():
+    if not flask.request.json:
+        flask.abort(400)
+
+    data = flask.request.json
+    print(flask.request.headers)
+    if "X-Github-Event" in flask.request.headers:
+        if flask.request.headers["X-Github-Event"] == "ping":
+            payload_to_influx(
+               "ping", data, data["organization"]["login"]
+            )
+            print(
+                "Pinged! Webhook created with id {}!".format(
+                    data["hook"]["id"]
+                )
+            )
+
+            result = client.query("select value from ping;")
+            print(result)
+            print(data)
+        elif flask.request.headers["X-Github-Event"] == "push":
+            print("No. commits in push:", len(data["commits"]))
+        elif flask.request.headers["X-Github-Event"] == "pull_requests":
+            print("PR", data["action"])
+            print("No. Commits in PR:", data["pull_request"]["commits"])
+
+    return "", 200
 
 
 def payload_to_influx(measurement, value, organisation, project=None):
     if not project:
         organisation = project
 
+    print(value)
     json_body = [
         {
             "measurement": measurement,
@@ -43,49 +72,3 @@ def payload_to_influx(measurement, value, organisation, project=None):
     ]
 
     client.write_points(json_body)
-
-
-@view_defaults(route_name=ENDPOINT, renderer="json", request_method="POST")
-class PayloadView(object):
-    def __init__(self, request):
-        self.request = request
-        self.payload = self.request.json
-
-    @view_config(header="X-Github-Event:push")
-    def payload_push(self):
-        print("No. commits in push:", len(self.payload["commits"]))
-        return Response("success")
-
-    @view_config(header="X-Github-Event:pull_request")
-    def payload_pull_request(self):
-        print("PR", self.payload["action"])
-        print("No. Commits in PR:", self.payload["pull_request"]["commits"])
-
-        return Response("success")
-
-    @view_config(header="X-Github-Event:ping")
-    def payload_else(self):
-        payload_to_influx(
-            "ping", self.payload, self.payload["organization"]["login"]
-        )
-        print(
-            "Pinged! Webhook created with id {}!".format(
-                self.payload["hook"]["id"]
-            )
-        )
-
-        result = client.query("select value from ping;")
-        print("Result: {0}".format(result))
-        return {"status": 200}
-
-
-if __name__ == "__main__":
-    config = Configurator()
-
-    config.add_route(ENDPOINT, "/{}".format(ENDPOINT))
-    config.scan()
-
-    app = config.make_wsgi_app()
-    server = make_server("0.0.0.0", 8000, app)
-
-    server.serve_forever()
