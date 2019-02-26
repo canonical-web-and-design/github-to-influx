afdf0f41c25657fc2bdc66dfea117b56dfb2ea1c
Thomas Bille <thomas.bille@canonical.com>
2019-02-26 10:30:27 +0000

    Add influxdb configuration

--- script.py
+++ script.py
@@ -1,36 +1,63 @@
 from __future__ import print_function
 
+import os
 from wsgiref.simple_server import make_server
+
+from influxdb import InfluxDBClient
 from pyramid.config import Configurator
-from pyramid.view import view_config, view_defaults
 from pyramid.response import Response
+from pyramid.view import view_config, view_defaults
 
 ENDPOINT = "webhook"
+INFLUXDB_CONFIG = {
+    "HOST": os.getenv("INFLUXDB_HOST", "localhost"),
+    "PORT": os.getenv("INFLUXDB_PORT", "8086"),
+    "USERNAME": os.getenv("INFLUXDB_USERNAME", ""),
+    "PASSWORD": os.getenv("INFLUXDB_PASSWORD", ""),
+    "DATABASE": os.getenv("INFLUXDB_DATABASE", "githubdb"),
+}
+
+
+client = InfluxDBClient(
+    INFLUXDB_CONFIG["HOST"],
+    INFLUXDB_CONFIG["PORT"],
+    INFLUXDB_CONFIG["PASSWORD"],
+    INFLUXDB_CONFIG["USERNAME"],
+    INFLUXDB_CONFIG["DATABASE"],
+)
+
+client.create_database("githubdb")
+
+
+def payload_to_influx(measurement, value, organisation, project=None):
+    if not project:
+        organisation = project
+
+    json_body = [
+        {
+            "measurement": measurement,
+            "tags": {"organisation": organisation, "project": project},
+            "time": value["hook"]["created_at"],
+            "fields": {"value": value},
+        }
+    ]
+
+    client.write_points(json_body)
 
 
 @view_defaults(route_name=ENDPOINT, renderer="json", request_method="POST")
 class PayloadView(object):
-    """
-    View receiving of Github payload. By default, this view it's fired only if
-    the request is json and method POST.
-    """
-
     def __init__(self, request):
         self.request = request
-        # Payload from Github, it's a dict
         self.payload = self.request.json
 
     @view_config(header="X-Github-Event:push")
     def payload_push(self):
-        """This method is a continuation of PayloadView process, triggered if
-        header HTTP-X-Github-Event type is Push"""
         print("No. commits in push:", len(self.payload["commits"]))
         return Response("success")
 
     @view_config(header="X-Github-Event:pull_request")
     def payload_pull_request(self):
-        """This method is a continuation of PayloadView process, triggered if
-        header HTTP-X-Github-Event type is Pull Request"""
         print("PR", self.payload["action"])
         print("No. Commits in PR:", self.payload["pull_request"]["commits"])
 
@@ -38,11 +65,17 @@ class PayloadView(object):
 
     @view_config(header="X-Github-Event:ping")
     def payload_else(self):
+        payload_to_influx(
+            "ping", self.payload, self.payload["organization"]["login"]
+        )
         print(
             "Pinged! Webhook created with id {}!".format(
                 self.payload["hook"]["id"]
             )
         )
+
+        result = client.query("select value from ping;")
+        print("Result: {0}".format(result))
         return {"status": 200}
 
 
@@ -54,4 +87,5 @@ if __name__ == "__main__":
 
     app = config.make_wsgi_app()
     server = make_server("0.0.0.0", 8000, app)
+
     server.serve_forever()
